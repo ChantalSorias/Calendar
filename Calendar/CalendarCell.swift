4d333b09dfb7831c1e25c7a4f2163b8d9ee30b64
Chantal Sorias <chantal_sorias@hotmail.com>
2025-01-09 14:55:40 -0500

    Calendar view: view days of month and go to previous or next month

---
+++ Calendar/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  Calendar
+//
+//  Created by Chantal on 2025-01-08.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
